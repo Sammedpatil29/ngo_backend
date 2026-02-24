2aa081b3288053fb1f863126f61031528b82aad8
Sammed <sammed.patil29@gmail.com>
2026-02-24 19:37:09 +0530

    cors disabled

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://ngo-navy.vercel.app'
-}));
+// app.use(cors({
+//   origin: 'https://ngo-navy.vercel.app'
+// }));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 // const allowedOrigin = 'https://ngo-navy.vercel.app';
