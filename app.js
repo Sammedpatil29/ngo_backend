352f1d6df1873048fe7e82ab2b73af60c2484ce1
Sammed <sammed.patil29@gmail.com>
2026-02-24 18:02:04 +0530

    cors added

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://ngo-navy.vercel.app'
+}));
 app.use(express.json({ limit: '50mb' }));
 
 // Database Connection
