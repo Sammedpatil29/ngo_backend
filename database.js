189af2cfcefb7a431eeb34db732242d847b22424
Sammed <sammed.patil29@gmail.com>
2026-04-12 17:12:28 +0530

    ssl removed

--- database.js
+++ database.js
@@ -6,10 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: process.env.DB_DIALECT || 'postgres',
   port: process.env.DB_PORT || 5432,
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+    ssl: false
   },
   logging: false
 });
